700154d8f164fb04cd6beaeef4b864f5c9e2295b
Faisal Faiz <faisalfaizff24@gmail.com>
2025-01-06 23:48:55 +0530

    add order and payment functionality; implement order details page, payment method selection, and order creation form; enhance validation and schema for orders and payment methods

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,12 +1,13 @@
 'use server'
 
 import { auth, signIn, signOut } from "@/auth"
-import { shippingAddressSchema, signInFormSchema, signUpFormSchema } from "../validator"
+import { paymentMethodSchema, shippingAddressSchema, signInFormSchema, signUpFormSchema } from "../validator"
 import { isRedirectError } from "next/dist/client/components/redirect-error"
 import { hashSync } from "bcrypt-ts-edge"
 import { prisma } from "@/db/prisma"
 import { formatError } from '../utils';
 import { ShippingAddress } from "@/types"
+import { z } from "zod"
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
     try {
         const user = signInFormSchema.parse({
@@ -87,6 +88,33 @@ export async function getUserById(userId: string) {
       await prisma.user.update({
         where: { id: currentUser.id },
         data: { address },
+      });
+  
+      return {
+        success: true,
+        message: 'User updated successfully',
+      };
+    } catch (error) {
+      return { success: false, message: formatError(error) };
+    }
+  }
+
+  // Update user's payment method
+export async function updateUserPaymentMethod(
+    data: z.infer<typeof paymentMethodSchema>
+  ) {
+    try {
+      const session = await auth();
+      const currentUser = await prisma.user.findFirst({
+        where: { id: session?.user?.id},
+      });
+      if (!currentUser) throw new Error('User not found');
+  
+      const paymentMethod = paymentMethodSchema.parse(data);
+  
+      await prisma.user.update({
+        where: { id: currentUser.id },
+        data: { paymentMethod: paymentMethod.type },
       });
   
       return {
