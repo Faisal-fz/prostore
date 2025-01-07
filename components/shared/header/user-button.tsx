2617dde1f83d348df70997fa9f3f7bee0145e13f
Faisal Faiz <faisalfaizff24@gmail.com>
2025-01-07 21:25:47 +0530

    add user layout and navigation components; implement user profile and orders pages;

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -28,42 +28,53 @@ const UserButton = async () => {
 
   return (
     <div className='flex gap-2 items-center'>
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className='flex items-center'>
-          <Button
-            variant='ghost'
-            className='relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-300'
-          >
-            {firstInitial}
-          </Button>
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className='w-56' align='end' forceMount>
-        <DropdownMenuLabel className='font-normal'>
-          <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>
-              {session.user?.name}
-            </p>
-            <p className='text-xs leading-none text-muted-foreground'>
-              {session.user?.email}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-  
-        <DropdownMenuItem className='p-0 mb-1'>
-          <form action={SignOutUser} className='w-full'>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className='flex items-center'>
             <Button
-              className='w-full py-4 px-2 h-4 justify-start'
               variant='ghost'
+              className='relativee w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-200'
             >
-              Sign Out
+              {firstInitial}
             </Button>
-          </form>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </div>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-56' align='end' forceMount>
+          <DropdownMenuLabel className='font-normal'>
+            <div className='flex flex-col space-y-1'>
+              <div className='text-sm font-medium leading-none'>
+                {session.user?.name}
+              </div>
+              <div className='text-sm text-muted-foreground leading-none'>
+                {session.user?.email}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+
+          <DropdownMenuItem>
+            <Link href='/user/profile' className='w-full'>
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href='/user/orders' className='w-full'>
+              Order History
+            </Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem className='p-0 mb-1'>
+            <form action={SignOutUser} className='w-full'>
+              <Button
+                className='w-full py-4 px-2 h-4 justify-start'
+                variant='ghost'
+              >
+                Sign Out
+              </Button>
+            </form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 };
 
