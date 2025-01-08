a23172b29564d6c6eee6c31f8a15d7c6c36bd396
Faisal Faiz <faisalfaizff24@gmail.com>
2025-01-08 22:41:47 +0530

    add profile update functionality; implement pagination for user orders; enhance validation schemas and add query-string utility

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,9 +1,19 @@
-const ProfilePage = () => {
-    return ( 
-        <>
-    Profile
-    </>
-     );
+import { Metadata } from 'next';
+import ProfileForm from './profile-form';
+import { SessionProvider } from 'next-auth/react';
+import { auth } from '@/auth';
+export const metadata: Metadata = {
+  title: 'Customer Profile',
+};
+
+export default async function ProfilePage() {
+    const session = await auth();
+    return (
+        <SessionProvider session={session}>
+          <div className='max-w-md  mx-auto space-y-4'>
+            <h2 className='h2-bold'>Profile</h2>
+            <ProfileForm />
+          </div>
+        </SessionProvider>
+      );
 }
- 
-export default ProfilePage;
--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -11,6 +11,7 @@ import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { convertToPlainObject } from '../utils';
 import { paypal } from '../paypal';
 import { revalidatePath } from 'next/cache';
+import { PAGE_SIZE } from '../constants';
 // Create an order
 export async function createOrder() {
   try {
@@ -232,3 +233,31 @@ async function updateOrderToPaid({
     throw new Error('Order not found');
   }
 };
+
+export  async function getMyOrders({
+  limit= PAGE_SIZE,
+  page
+}: {limit?: number, page: number}) {
+  const session = await auth();
+  if (!session) throw new Error('User is not authenticated');
+  
+  const data = await prisma.order.findMany({
+    where:{
+      userId: session?.user?.id
+    },
+    orderBy:{
+      createdAt: 'desc'
+    },
+    take: limit,
+    skip: limit * (page - 1)
+  })
+  const dataCount = await prisma.order.count({
+    where: {
+      userId: session?.user?.id
+    }
+  })
+  return {
+    data,
+    totalPages : Math.ceil(dataCount / limit)
+  }
+}
